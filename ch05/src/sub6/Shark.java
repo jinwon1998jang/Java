3bd279aca334b5f05ca62bcbdbcb0406e02f18fe
jinwonj96 <jinwonj96@naver.com>
2025-07-15 12:27:34 +0900

    add ch05

---
+++ ch05/src/sub6/Shark.java
@@ -0,0 +1,18 @@
+package sub6;
+
+public class Shark extends Animal{
+
+	@Override
+	public void move() {
+		System.out.println("Shark swim...");
+	}
+	
+	@Override
+	public void hunt() {
+		System.out.println("Shark hunt...");
+	}
+	
+	public void eat() {
+		System.out.println("Shark eat...");
+	}
+}
