e60892040c2fe1d305348d810c2b4af69f8027cb
jinwonj96 <jinwonj96@naver.com>
2025-07-17 16:46:44 +0900

    add javatest

--- JavaTest/src/test4/sub5/Student.java
+++ JavaTest/src/test4/sub5/Student.java
@@ -28,7 +28,7 @@ public class Student {
 
 	    public void setScore(Subject subject, int score) {
 	        for (int i = 0; i < subjectCount; i++) {
-	            if (subjects[i].equals(subject)) {
+	            if (subjects[i]==subject) {
 	                scores[i] = score;
 	                System.out.println(name + " - " + subject.getName() + " 점수 입력완료");
 	                return;
